27588687dbbe6a40503515ee5c07187e9502aa4e
qyz <756913602@qq.com>
2020-06-21 21:17:29 +0200

    Create weather.test.ts

---
+++ src/weather.test.ts
@@ -0,0 +1,34 @@
+import { Weather } from './weather'
+
+describe('class Weather', () => {
+  let weather: Weather
+  test('class Weather constructer', () => {
+    weather = new Weather('Vienna')
+    expect(weather).toBeInstanceOf(Weather)
+  })
+
+  test('weather.getTemp() ', async () => {
+    expect(typeof weather.getTemp).toBe('function')
+    expect(typeof await weather.getTemp()).toBe('number')
+    expect(isNaN(await weather.getTemp() as number)).toBeFalsy()
+  })
+
+  test('weather.getTemp() ', async () => {
+    expect(typeof weather.getWind).toBe('function')
+    const res = await weather.getWind()
+    expect(typeof res).toBe('object')
+    expect(typeof res?.direction).toBe('number')
+    expect(typeof res?.speed).toBe('number')
+  })
+
+  test('weather.getForecasts()', async () => {
+    expect(typeof weather.getForecasts).toBe('function')
+    const res = await weather.getForecasts()
+    expect(res?.length).toBe(7)
+    for (const _res of res!) {
+      expect(typeof _res.temp).toBe('number')
+      expect(typeof _res.wind.speed).toBe('number')
+      expect(typeof _res.wind.direction).toBe('number')
+    }
+  })
+})
