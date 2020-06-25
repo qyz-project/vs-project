7ca9013ca9fa4f576479ca9dab81946322b3fbcc
qyz <756913602@qq.com>
2020-06-25 14:58:45 +0200

    client stab

---
+++ src/weather-ws-client.ts
@@ -0,0 +1,83 @@
+// use web socket api of browser
+import config from './config'
+
+import axios from 'axios'
+import config from './config'
+import { Forcast, WindState } from './WeatherData'
+
+// Create WebSocket connection.
+const socket = new WebSocket(`ws://localhost:${config.port.weather}`)
+
+/**
+ * wait when cocket open
+ * @param message message
+ */
+async function open (): Promise<void> {
+  await new Promise((resolve, reject) => {
+    socket.addEventListener('open', function (event) {
+      resolve()
+    }, { once: true })
+  })
+}
+
+/**
+ * send a message to remote
+ * @param message message
+ */
+function send (message: string) {
+  socket.send(message)
+}
+
+async function get (): Promise<string> {
+  return await new Promise<string>((resolve, reject) => {
+    socket.addEventListener('message', function (event) {
+      resolve(event.data)
+    }, { once: true })
+  })
+}
+
+/**
+ * this class handle everything abould weather
+ * it use server from https://openweathermap.org/
+ */
+export class Weather {
+  /**
+   * default api for https://openweathermap.org/
+   */
+  static readonly DEFAULT_API_KEY = config.api.weather
+
+  /**
+   * set curretn city
+   * @param city city
+   */
+  setCity (city: string) {
+    send(`city=${city}`)
+  }
+
+  /**
+   * get current temperature
+   * @returns temperature (in promise)
+   */
+  async getTemp (): Promise<number | undefined> {
+    send('temp')
+    return Number(await get())
+  }
+
+  /**
+   * get current wind state
+   * @returns temperature (in promise)
+   */
+  async getWind (): Promise<WindState | undefined> {
+    send('wind')
+    return JSON.parse(await get())
+  }
+
+  /**
+   * get forecast of future 7 days
+   * @returns array of temperature and wind state (in promise)
+   */
+  async getForecasts (): Promise<Array<Forcast> | undefined> {
+    send('forecast')
+    return JSON.parse(await get())
+  }
+}
