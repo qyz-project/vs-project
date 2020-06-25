8e36be907161876599151917d9df084ab0e1e938
qyz <756913602@qq.com>
2020-06-26 01:39:14 +0200

    rest server for smart home

---
+++ src/smarthome-rest-server.ts
@@ -0,0 +1,75 @@
+import express from 'express'
+import * as parser from 'body-parser'
+import * as api from './smarthome-store-api'
+
+const app = express()
+
+app.use(parser.urlencoded({ extended: false }))
+
+// get all device info
+app.get('/device', (req, res) => {
+  res.end(JSON.stringify(api.getDevices(), undefined, 4))
+})
+
+// get all room info
+app.get('/room', (req, res) => {
+  res.end(JSON.stringify(api.getRooms(), undefined, 4))
+})
+
+// get a device info
+app.get('/device/:id', (req, res) => {
+  const d = api.getDevice(Number(req.params.id))
+  res.end(JSON.stringify(d, undefined, 4))
+})
+
+// remove a device
+app.delete('/device/:id', (req, res) => {
+  api.removeDevice(Number(req.params.id))
+  res.end(JSON.stringify(api.getDevices(), undefined, 4))
+})
+
+// set value of a device
+app.put('/device/:id/value/:value', (req, res) => {
+  api.setDevice(Number(req.params.id), Number(req.params.value))
+  const d = api.getDevice(Number(req.params.id))
+  res.end(JSON.stringify(d, undefined, 4))
+})
+
+// add new device
+app.post('/device', (req, res) => {
+  const d = api.addDevice(JSON.parse(req.body))
+  res.end(JSON.stringify(d, undefined, 4))
+})
+
+// get device from room
+app.get('/room/:roomId/', (req, res) => {
+  const d = api.getRoom(Number(req.params.roomId))
+  res.end(JSON.stringify(d, undefined, 4))
+})
+
+// get device from room
+app.get('/room/:roomId/device/', (req, res) => {
+  const d = api.roomGetDevices(Number(req.params.roomId))
+  res.end(JSON.stringify(d, undefined, 4))
+})
+
+// add device to room
+app.put('/room/:roomId/device/:id', (req, res) => {
+  api.roomAddDevice(Number(req.params.roomId), Number(req.params.id))
+  const d = api.getRoom(Number(req.params.roomId))
+  res.end(JSON.stringify(d, undefined, 4))
+})
+
+// remove device from room
+app.delete('/room/:roomId/device/:id', (req, res) => {
+  api.roomRemoveDevice(Number(req.params.roomId), Number(req.params.id))
+  const d = api.getRoom(Number(req.params.roomId))
+  res.end(JSON.stringify(d, undefined, 4))
+})
+
+async function main () {
+  await api.main()
+  app.listen(8082)
+}
+
+main()
---
+++ src/smarthome-store-api.ts
@@ -0,0 +1,119 @@
+import { IStore, IDeviceWithoutId, IDevice, IRoom } from './SmartDevice'
+import * as fs from 'fs-extra'
+
+const dataFile = 'data/data.json'
+
+let data: IStore = {
+  rooms: [], devices: []
+}
+
+let motified = false
+
+async function start () {
+  try {
+    data = JSON.parse((await fs.readFile(dataFile)).toString())
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+async function sync () {
+  if (!motified) return
+  try {
+    await fs.writeFile(dataFile, JSON.stringify(data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export function addDevice (d: IDeviceWithoutId): void {
+  let id = 0
+  if (data.devices) {
+    for (const device of data.devices) {
+      id = Math.max(device.id, id)
+    }
+    id++
+  }
+  data.devices.push({ ...d, id } as IDevice)
+  motified = true
+}
+
+export function removeDevice (id: number): void {
+  data.devices = data.devices.filter((d) => d.id !== id)
+}
+
+export function getDevice (id: number): IDevice | void {
+  if (data.devices) {
+    for (const device of data.devices) {
+      if (device.id === id) return device
+    }
+  }
+  return undefined
+}
+
+export function getRoom (id: number): IRoom | void {
+  if (data.devices) {
+    for (const room of data.rooms) {
+      if (room.id === id) return room
+    }
+  }
+  return undefined
+}
+
+export function setDevice (id: number, value: number): void {
+  if (data.devices) {
+    for (const device of data.devices) {
+      if (device.id === id) device.value = value
+      motified = true
+    }
+  }
+}
+
+export function roomAddDevice (roomId: number, id: number): void {
+  if (data.rooms) {
+    for (const room of data.rooms) {
+      if (room.id === roomId) {
+        if (room.devices.indexOf(id) < 0) {
+          room.devices.push(id)
+          motified = true
+        }
+      }
+    }
+  }
+}
+
+export function roomRemoveDevice (roomId: number, id: number): void {
+  if (data.rooms) {
+    for (const room of data.rooms) {
+      if (room.id === roomId) {
+        room.devices = room.devices.filter((_id) => _id !== id)
+        motified = true
+      }
+    }
+  }
+}
+
+export function roomGetDevices (roomId: number): Array<number> | void {
+  if (data.rooms) {
+    for (const room of data.rooms) {
+      if (room.id === roomId) {
+        return room.devices
+      }
+    }
+  }
+}
+
+export function getRooms (): IRoom[] {
+  return data.rooms
+}
+
+export function getDevices (): IDevice[] {
+  return data.devices
+}
+
+export async function main () {
+  await start()
+  setInterval(() => {
+    sync()
+  }, 5000)
+}
