6e52eea56375f6959c06dae7e5c668bc5e41dc90
qyz <756913602@qq.com>
2020-06-26 00:33:17 +0200

    Update SmartDevice.ts

--- src/SmartDevice.ts
+++ src/SmartDevice.ts
@@ -1,33 +1,20 @@
-export interface IDevice {
-    id: number
-    name: string
+export interface IDeviceWithoutId {
+  type: 'light' | 'dimmer' | 'jalousien'
+  name: string
+  value: number
 }
 
-export interface ILight extends IDevice {
-    type: 'light'
-    value: boolean
+export interface IDevice extends IDeviceWithoutId {
+  id: number
 }
 
-export interface IDimmer extends IDevice {
-    type: 'dimmer'
-    id: number
-    name: string
-    value: number // 0 - 1
+export interface IRoom {
+  name: string
+  id: number
+  diveces: Array<number>
 }
 
-export interface IJalousien extends IDevice {
-    type: 'jalousien'
-    id: number
-    name: string
-    value: number // 0 -1
-}
-
-export interface Room {
-    name: string
-    id: number
-    diveces: Array<IDevice>
-}
-
-export interface Store {
-    [key: string]: Array<ILight| IJalousien | IDimmer>
+export interface IStore {
+  rooms: IRoom[]
+  devices: IDevice[]
 }
