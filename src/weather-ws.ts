7abce74c7cd26114a8236a9704c15a40f8a7d6d4
qyz <756913602@qq.com>
2020-06-25 14:58:37 +0200

    server stab

---
+++ src/weather-ws.ts
@@ -0,0 +1,60 @@
+import WebSocket, { Server } from 'ws'
+import config from './config'
+import { Weather } from './weather'
+
+// just use demo code from https://www.npmjs.com/package/ws
+const server = new Server({
+  port: config.port.weather,
+  perMessageDeflate: {
+    zlibDeflateOptions: {
+      // See zlib defaults.
+      chunkSize: 1024,
+      memLevel: 7,
+      level: 3
+    },
+    zlibInflateOptions: {
+      chunkSize: 10 * 1024
+    },
+    // Other options settable:
+    clientNoContextTakeover: true, // Defaults to negotiated value.
+    serverNoContextTakeover: true, // Defaults to negotiated value.
+    serverMaxWindowBits: 10, // Defaults to negotiated value.
+    // Below options specified as default values.
+    concurrencyLimit: 10, // Limits zlib concurrency for perf.
+    threshold: 1024 // Size (in bytes) below which messages
+    // should not be compressed.
+  }
+})
+
+const weather = new Weather()
+
+server.on('connection', (client) => {
+  client.on('message', (message) => {
+    const m: string = String(message)
+
+    if (m.match('=')) {
+      if (m.match('=')) {
+        const [name, value] = m.split('=')
+        switch (name) {
+          case 'city': {
+            weather.setCity(value)
+          }
+        }
+      }
+    } else {
+      switch (m) {
+        case 'temp': {
+          client.send(JSON.stringify(weather.getTemp()))
+          return
+        }
+        case 'wind': {
+          client.send(JSON.stringify(weather.getWind()))
+          return
+        }
+        case 'forecast': {
+          client.send(JSON.stringify(weather.getForecasts()))
+        }
+      }
+    }
+  })
+})
