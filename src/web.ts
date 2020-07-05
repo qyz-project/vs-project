aeb5dbffd5527234d285252de959820ef8b6652d
qyz <756913602@qq.com>
2020-07-05 15:32:16 +0200

    Create web.ts

---
+++ src/web.ts
@@ -0,0 +1,32 @@
+import express from 'express'
+import next from 'next'
+import config from './config'
+import { resolve } from 'path'
+
+const port = config.port.web
+const app = next({
+  dir: resolve(__dirname, './web'),
+  dev: true
+})
+
+const handle = app.getRequestHandler()
+
+let server: express.Express
+app
+  .prepare()
+  .then(() => {
+    server = express()
+    server.all('*', (req, res) => handle(req, res))
+    server.listen(port, (err) => {
+      if (err) {
+        console.error(err)
+        process.exit(2)
+      }
+      console.log(`> Ready on port ${port}`)
+    })
+  })
+  .catch((err) => {
+    console.error('An error occurred, unable to start the server')
+    console.error(err)
+    process.exit(1)
+  })
