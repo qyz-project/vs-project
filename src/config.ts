f620634c90a8a490bc85f8306b2534b47ff58531
qyz <756913602@qq.com>
2020-07-05 15:32:13 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,8 @@ export default {
   host: 'localhost',
   port: {
     weather: 8081,
-    smart: 8082
+    smart: 8082,
+    web: 8080
   },
   api: {
     weather: '47ebbc8d3c411548c79f8954777f9792'
