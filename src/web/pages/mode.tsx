0cdc00ddaa9646eecbdeb80cc79218832e4c71f1
qyz <756913602@qq.com>
2020-07-06 15:57:53 +0200

    better looking now

--- src/web/pages/mode.tsx
+++ src/web/pages/mode.tsx
@@ -7,33 +7,38 @@ import Container from 'react-bootstrap/Container'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import axios from 'axios'
 import config from '../../config'
+import Col from 'react-bootstrap/Col'
 
 const rootUrl = 'http://' + config.host + ':' + config.port.smart
 
-export default function WeatherPage () {
+export default function ModePage () {
   const devices = [
     'read',
     'sleep',
     'party',
     'stop-party'
   ].map((mode, i) => {
-    return <Card key={String(i)} style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title>{mode}</Card.Title>
-        <Button variant="primary" onClick={() => {
-          axios.put(rootUrl + '/mode')
-        }}>run</Button>
-      </Card.Body>
-    </Card>
+    return <Col md={4} key={String(i)}>
+      <Card className="mb-3">
+        <Card.Body>
+          <Card.Title>{mode}</Card.Title>
+          <Button variant="primary" onClick={() => {
+            axios.put(rootUrl + '/mode/' + mode)
+          }}>Toggle Mode</Button>
+        </Card.Body>
+      </Card>
+    </Col>
   })
 
   return (
     <Page>
       <Container>
-        <Row md={1}>
-          <Jumbotron>
-            <h1>Mode</h1>
-          </Jumbotron>
+        <Row>
+          <Col>
+            <Jumbotron>
+              <h1>Mode</h1>
+            </Jumbotron>
+          </Col>
         </Row>
         <Row md={4}>
           {devices}
